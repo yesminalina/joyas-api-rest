5a01210c87a44cdc85af8933a92bf059a01e3a83
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2024-06-15 21:29:58 -0400

    findAll model

--- src/models/joyas.dao.js
+++ src/models/joyas.dao.js
@@ -1,3 +1,11 @@
 import format from 'pg-format'
 import db from '../database/db_connect.js'
 
+export const findAll = ({ limits = 10, order_by: orderBy = 'stock_ASC', page = 1 }) => {
+  const query = 'SELECT * FROM inventario'
+  console.log(query)
+  const [column, sort] = orderBy.split('_')
+  const offset = Math.abs(+page !== 0 ? page - 1 : 0) * limits
+  const formattedQuery = format(`${query} ORDER BY %s %s LIMIT %s OFFSET %s;`, column, sort, limits, offset)
+  return db(formattedQuery)
+}
